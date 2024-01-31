a3a00859dc00f4356af8596c32de38126a835c23
tcruz4728 <thomas.cruz03@utrgv.edu>
2024-01-31 14:50:53 -0600

    Ls6 Updates

--- dataRealizationgenmat.m
+++ dataRealizationgenmat.m
@@ -1,12 +1,13 @@
-dataGenType = input("Data Generation Type: ");
-switch dataGenType
+params = loadjson('JSON\realizations.json');
+
+switch params.dataGenType
     case 1 %Data Loading from externally generated files
-        load("data_realizations_10Nov2023.mat")  
+        load(params.extGenFilename)  
         PSD = sigparams.PSD;
         frange = sigparams.frange;
     case 2 %Data Created from sensitivity data
         % userNrealizations = input("Set the number of data realizations to be generated: ");
-        sigparams = loadjson('JSON\realizations.json');
+        sigparams = params;
         addpath(sigparams.path2sensitivitydata)
         Nsamples = sigparams.sampFreq*sigparams.datalen;
         data_realizations = zeros(sigparams.nRealizations,Nsamples);
@@ -18,9 +19,13 @@ tIntrvl = 1/sigparams.sampFreq;
 
 for i = 1:size(data_realizations,1)
     dataY = data_realizations(i,:);
-    save(['TMPPSDDATA\test',num2str(i),'_',num2str(sigparams.datalen),...
+    save(['TMPPSDDATA',filesep,test',num2str(i),'_',num2str(sigparams.datalen),...
         's_inj',num2str(sigparams.ta),'_fs',num2str(sigparams.sampFreq),'.mat'],...
         "dataY","tIntrvl",'PSD')
 end
-disp(['Saved ',num2str(size(data_realizations,1)),' data files to Accelerated-Network-Analysis\TMPPSDDATA\ as test<#>_',num2str(sigparams.datalen),...
-        's_inj',num2str(sigparams.ta),'_fs',num2str(sigparams.sampFreq),'.mat'])
+disp(['Saved ',num2str(size(data_realizations,1)),...
+    ' data files to Accelerated-Network-Analysis',filesep,'TMPPSDDATA',...
+    filesep,...
+    ' as test<#>_',num2str(sigparams.datalen),...
+        's_inj',num2str(sigparams.ta),...
+        '_fs',num2str(sigparams.sampFreq),'.mat'])
