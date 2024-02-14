0b264cadf956ffc1b2cf5371c0dc657f826dccc0
tcruz4728 <thomas.cruz03@utrgv.edu>
2024-02-13 23:16:40 -0600

    Update combmultimfshps.m

--- combmultimfshps.m
+++ combmultimfshps.m
@@ -33,7 +33,7 @@ outFilesList = readcell(outFileList,"Delimiter","  ");
 % paramsFilesList = readcell([outdataFilePrfx,'_paramsFilesList.txt'],"Delimiter","  ");
 
 %Time series initial file
-inFileNameList = inFilesList{:,1};
+inFileNameList = inFilesList{:,1}
 %Pwelch data file (inFilePSD-goes into SHAPES)
 interFileNameList = inFilesList{:,2};
 %SHAPES estimated PSD data file (inFileshpsPSD)
